9f0b1baf785cd51f4e2671295b1739ce2a75824c
drJAGartner <dr.j.a.gartner@gmail.com>
2015-11-18 17:50:09 -0800

    progress towards alpha prototype

--- analyze_tweet_file.py
+++ analyze_tweet_file.py
@@ -1,12 +1,40 @@
 import os
 import sys
+import elasticsearch
 sys.path.insert(0, './lib/')
 from clustering import ScoreRecord, ScoreBin
 
-def analyze_recent(tweet_file_path):
+def analyze_recent(tweet_file_path, es_url=None):
+    es = None
+    if es_url == None:
+        es = elasticsearch.Elasticsearch()
+    else:
+        es = elasticsearch.Elasticsearch([es_url])
+
     files = sorted(os.listdir(tweet_file_path), key=lambda x: os.stat(os.path.join(tweet_file_path, x)).st_mtime)
-    dict_all_hastags = {}
+    new_records = {}
     for file in files:
-        d0 = open(file)
+        print "analyzing file:", file
+        d0 = open(tweet_file_path + "/" + file)
         for line in d0:
             sr = ScoreRecord(line)
+            for tag in sr.tags:
+                if tag in new_records.keys():
+                    new_records[tag].append(sr)
+                else:
+                    new_records[tag] = [sr]
+        break
+
+    for tag, lst_rec in new_records.keys():
+        print "Getting data for tag: ", tag
+        count = es.count(index="jag_hc2_documents", doc_type="post", q="tags:"+tag)["count"]
+        #test if there is enough entries for clustering
+        if (count+len(lst_rec))<5:
+            for sr in lst_rec:
+                sr.write_to_es("jag_hc2_documents","post",es)
+            continue
+        #query ES to get previous entries with the same tags from the last 8 hours
+        #associate querries with the existing hashtag list
+        #perform clustering on larger list
+        #execute steps on case based list
+
--- lib/clustering.py
+++ lib/clustering.py
@@ -1,6 +1,11 @@
 import json
 import pytz
+import re
+import elasticsearch
 from datetime import datetime, timedelta
+from sklearn.cluster import DBSCAN
+from sklearn.preprocessing import StandardScaler
+from math import sqrt
 
 def utc_to_local(utc_dt):
     # get integer timestamp to avoid precision lost
@@ -9,45 +14,79 @@ def utc_to_local(utc_dt):
     assert utc_dt.resolution >= timedelta(microseconds=1)
     return local_dt.replace(microsecond=utc_dt.microsecond)
 
+def text_to_hashtags(caption):
+    term_list = re.sub('[^\w\s#]', '', caption, flags=re.UNICODE).lower().split()
+    ret_list = set()
+    for term in term_list:
+        try:
+            if term[0]=="#":
+                if term.find("#",1)==-1:
+                    if len(term) > 2:
+                        ret_list.add(term[1:])
+                else:
+                    subs = term.split("#")
+                    for sub in subs:
+                        if len(sub) > 2:
+                            ret_list.add(sub)
+        except:
+            continue
+    return list(ret_list)
+
+def assignToCluster(recordList, epsilon, n_min):
+    lalo = []
+    for line in recordList:
+        lalo.append([line.lon, line.lat])
+
+    X = StandardScaler().fit_transform(lalo)
+    fitObj = StandardScaler().fit(lalo)
+    laEps = epsilon/fitObj.std_[0]
+    loEps = epsilon/fitObj.std_[1]
+    fitEps = sqrt(laEps*laEps+loEps*loEps)
+    db = DBSCAN(eps=fitEps, min_samples=n_min).fit(X)
+    for ind in range(len(recordList)):
+        recordList[ind].cluster = db.labels_[ind]
+
 class ScoreRecord:
-    def __init__(self, json_data):
-        record = json.loads(json_data)
-        self.lat = record["geo"]["coordinates"][0]
-        self.lon = record["geo"]["coordinates"][1]
-        self.text = record["text"]
-        self.username = ["user"]["screen_name"]
-        self.dt = utc_to_local(datetime.strptime(record["created_at"],'%a %b %d %H:%M:%S +0000 %Y'))
-        self.img = record.img
-        self.cluster = -1
+    def __init__(self, json_data, data_type=0):
+        if data_type==0:
+            record = json.loads(json_data)
+            self.id = record["id_str"]
+            self.lat = record["geo"]["coordinates"][0]
+            self.lon = record["geo"]["coordinates"][1]
+            self.text = record["text"]
+            self.username = record["user"]["screen_name"]
+            self.tags = text_to_hashtags(record["text"])
+            self.dt = utc_to_local(datetime.strptime(record["created_at"],'%a %b %d %H:%M:%S +0000 %Y'))
+            self.cluster = -1
+            self.cluster_ind = ""
 
     def toDict(self):
         obj = {
-            'date': str(self.dt.date()),
-            'datetime':str(self.dt),
-            'cap': self.text,
-            'usr': self.username,
-            'lon': self.lon,
-            'lat': self.lat,
-            'cluster': self.cluster
+            'id': self.id,
+            'user': self.username,
+            'caption': self.text,
+            'tags':self.tags,
+            'post_date': str(self.dt.date())+"T"+str(self.dt.hour)+":"+str(self.dt.minute)+":"+str(self.dt.second)+"Z",
+            'location':{
+                "type":"point",
+                "coordinates":[self.lon, self.lat]
+            }
         }
-        if self.img is not None and len(self.img) > 0:
-            obj['img'] = self.img
         return obj
+
+    def write_to_es(self, es_index, es_doc_type, es):
+        mapped = self.toDict()
+        es.index(index=es_index, doc_type=es_doc_type, id=self.id, body=json.dumps(mapped))
+
+
 
 class ScoreBin:
     def __init__(self, record=None):
         self.users = set([])
-        self.lat = ''
-        self.lon = ''
-        self.dt = None
         self.records = []
-        self.poly = []
-        self.objPoly = None
-        self.postsInHull = -1
+        self.n_clusters = -1
+        self.clusters = []
         if record is not None:
-            self.lat = record.lat
-            self.lon = record.lon
-            self.dt = record.dt
             self.records.append(record)
             self.users.add(record.username)
 
@@ -64,8 +103,11 @@ class ScoreBin:
             'lat': self.lat,
             'lon': self.lon,
             'nUnique': len(self.users),
-            'nTotal': len(self.records),
-            'poly': list(self.poly),
-            'background': self.postsInHull
-            #'posts': map(lambda x: x.toDict(), self.records)
+            'nTotal': len(self.records)
         }
+
+    def determine_clusters(self, epsilon, n_min):
+        assignToCluster(self.records, epsilon, n_min)
+        #clustered_recs = filter(lambda x: x.cluster != -1, self.records)
+        #cluster_nums = set()
+        #for record in self.records:
