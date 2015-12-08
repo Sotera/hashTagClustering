928d32b2ad3d02705a5c429094573a10cdb31eb9
drJAGartner <dr.j.a.gartner@gmail.com>
2015-12-08 11:16:36 -0800

    add list reading of authentification to avoid timeout

--- streamTwitter.py
+++ streamTwitter.py
@@ -3,18 +3,21 @@ import oauth2 as oauth
 import urllib2 as urllib
 import datetime
 import os
-from math import sqrt
 
 def getCredentials():
     #you'll need to get these by registering for your own twitter developer account
+    #i've created multiple access keys to loop through to avoid timeout
     dict_llaves = json.load(open("private/keyFile.json"))
-    api_key = dict_llaves["consumer_key"]
-    api_secret = dict_llaves["consumer_secret"]
-    access_token_key = dict_llaves["access_token"]
-    access_token_secret = dict_llaves["access_secret"]
-    oauth_token    = oauth.Token(key=access_token_key, secret=access_token_secret)
-    oauth_consumer = oauth.Consumer(key=api_key, secret=api_secret)
-    return (oauth_token, oauth_consumer)
+    auth_info = []
+    for llave in dict_llaves:
+        api_key = llave["consumer_key"]
+        api_secret = llave["consumer_secret"]
+        access_token_key = llave["access_token"]
+        access_token_secret = llave["access_secret"]
+        oauth_token    = oauth.Token(key=access_token_key, secret=access_token_secret)
+        oauth_consumer = oauth.Consumer(key=api_key, secret=api_secret)
+        auth_info.append((oauth_token, oauth_consumer))
+    return auth_info
 
 def twitterreq(oauth_token, oauth_consumer, url, http_method, parameters):
     http_handler  = urllib.HTTPHandler(debuglevel=0)
@@ -35,28 +38,36 @@ def twitterreq(oauth_token, oauth_consumer, url, http_method, parameters):
     return response
 
 def main():
-    (oauth_token, oauth_consumer) = getCredentials()
+    auth_info = getCredentials()
+    auth_counter = 0
 
     http_method = "GET"
 
     #url = "https://stream.twitter.com/1.1/statuses/filter.json?track=MakeAmericaMoreAmerican&locations=-88.195284,41.562522,-87.507085,42.136065"
-    url = "https://stream.twitter.com/1.1/statuses/filter.json?locations=-88.195284,41.562522,-87.507085,42.136065"
+    url = "https://stream.twitter.com/1.1/statuses/filter.json?stall_warnings=true&locations=-88.195284,41.562522,-87.507085,42.136065"
 
     pars = []
     current_block = datetime.datetime.now()
     current_string = str(current_block.date())+"_"+str(current_block.time())+".json"
     out_file = open("./raw_tweet_data/live_stream/"+current_string,"w")
-    response = twitterreq(oauth_token, oauth_consumer, url, http_method, pars)
+    response = twitterreq(auth_info[auth_counter][0], auth_info[auth_counter][1], url, http_method, pars)
+    response_open_time = datetime.datetime.now()
 
     for line in response:
         now = datetime.datetime.now()
         diff = now - current_block
-        if diff.seconds > 180:
+        if diff.seconds > 180 :
             out_file.close()
             os.rename("raw_tweet_data/live_stream/"+current_string, "raw_tweet_data/"+current_string)
             current_block = now
             current_string = str(current_block.date())+"_"+str(current_block.time())+".json"
             out_file = open("./raw_tweet_data/live_stream/"+current_string,"w")
+            #every 2 hours, close existing connection, open under new key to avoid timeout
+            response_up_time = now - response_open_time
+            if response_up_time.seconds > 7200:
+                response.close()
+                auth_counter = (auth_counter+1)%len(auth_info)
+                response = twitterreq(auth_info[auth_counter][0], auth_info[auth_counter][1], url, http_method, pars)
         try:
             dic_line = json.loads(line)
             if (dic_line["geo"] is not None or dic_line["coordinates"] is not None) and len(dic_line["entities"]["hashtags"])!=0:
